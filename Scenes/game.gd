32608e63b37c169f3b58cb2dc9d3b063cc355ebd
William Scholder <william.scholder@edu.hefr.ch>
2024-05-03 19:05:45 +0200

    add : scene implementation

---
+++ Scenes/game.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var time_start = 0
+var time_now = 0
+
+func _ready():
+	time_start = Time.get_ticks_msec() / 1000
+	print(time_start)
+
+func _on_player_player_took_damage(health):
+	%HealthLabel.text = "health : %" % health
+	if health == 0:
+		game_over()
+
+func _process(delta):
+	time_now = Time.get_ticks_msec() / 1000
+	%TimeElapsedLabel.text = "time : %s" % (time_now - time_start)
+
+func game_over():
+	%GameOverScreen.visible = true
+	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,16 +1,60 @@
 extends CharacterBody2D
 
 
-func _physics_process(delta):
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 300
-	move_and_slide()
-	if Input.is_action_just_pressed("dash"):
-		dash()
+var health = 5
 
-		
-func dash():
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 600
-	move_and_slide()
+const LOADING_DASH_TIME = 2
+var load_dash_acc = 0
+
+signal player_took_damage
+
+var direction = Vector2(0,0)
+var dashing = false
+const DASH_TIME = 0.2
+var dash_acc = 0
+var rotation_speed = 2
+var screen_size = Vector2i(0,0)
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func take_damage():
+	health -= 1
+	player_took_damage.emit(health)
 	
+
+func _physics_process(delta):
+	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	# prevent player going out of screen
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if dashing == false:
+		velocity = direction * 300
+	move_and_slide()
+	look_at(get_global_mouse_position())
+	
+func _input(event):
+	if dashing: return
+	
+	if Input.is_action_pressed('dash'):
+		if load_dash_acc >= LOADING_DASH_TIME:
+			print("Dashing")
+			velocity = direction * 800
+			dashing = true
+			load_dash_acc = 0
+		
+func _process(delta):
+	if dashing:
+		dash_acc += delta
+	else:
+		load_dash_acc += delta
+
+	if dash_acc >= DASH_TIME:    
+		dashing = false
+		velocity = direction * 400
+		dash_acc = 0
+	
+	
+
